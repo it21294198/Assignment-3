f7a11bf8f751a6df07aed0c8bd5484f167e02767
chamath-shanaka <chamathshanaka19@gmail.com>
2025-05-22 20:42:15 +0530

    Added court

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 window.addEventListener('DOMContentLoaded', () => {
   const slider = document.getElementById('sizeSlider');
   const model = document.getElementById('model');
+  this.court = document.querySelector('#tennis-court');
 
   slider.addEventListener('input', (e) => {
     const scale = e.target.value;
